ef8f8314c18c38640e0cffd0b88fdfebac4ec4a8
Kim Gowans <gowa0019@algonquinlive.com>
2018-04-03 18:46:16 -0400

    Exemption added to Grade Value array

--- src/funcs.php
+++ src/funcs.php
@@ -198,7 +198,7 @@ function val_Search($f_title, $f_name) {
 
 function gradeValue($grade) {
     $lookup = array(
-        "A+" => 12, "A" => 11, "A-" => 10,
+        "EX" => 13, "A+" => 12, "A" => 11, "A-" => 10,
         "B+" => 9, "B" => 8, "B-" => 7,
         "C+" => 6, "C" => 5, "C-" => 4,
         "D+" => 3, "D" => 2, "D-" => 1,
